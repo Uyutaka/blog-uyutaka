e916571ad68119001d429c992e9ce7ee1e6318c9
uyutaka <yutaka.urakami@colorado.edu>
2023-03-29 06:39:02 +0000

    Support code highlight and toc

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,22 @@
 import { remark } from 'remark'
+import remarkPrism from 'remark-prism'
+import remarkToc from 'remark-toc'
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeSlug from "rehype-slug";
+import { unified } from 'unified'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await unified()
+    .use(html)
+    .use(remarkToc)
+    .use(remarkParse)
+    .use(remarkPrism, { plugins: ["line-numbers"] })
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .use(rehypeSlug)
+    .process(markdown);
   return result.toString()
 }
